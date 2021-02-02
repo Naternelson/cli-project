1a3c82bc86154f6ce4edcf663a5e6064edbd7ff2
Nate <7nater.nelson@gmail.com>
2021-02-02 11:55:31 -0700

    Add initialize and make_chord method

--- lib/chord.rb
+++ lib/chord.rb
@@ -1,4 +1,42 @@
 class Chord
+    NOTES = {
+        "A" => 0,
+        "A# / Bb" => 1,
+        "B" => 2,
+        "C" => 3,
+        "C# / Db" => 4,
+        "D" => 5,
+        "D# / Eb" => 6,
+        "E" => 7,
+        "F" => 8,
+        "F# / Gb" => 9,
+        "G" => 10,
+        "G# / Ab" => 11
+    }
+    CHORDS = {
+        "I" => {type: "Major", notes: [0, 4,7]},
+        "ii" => {type: "Minor", notes: [2, 5, 9]},
+        "iii" => {type: "Minor", notes: [4, 7, 11]},
+        "iv" => {type: "Major", notes: [5, 9, 0]},
+        "V" => {type: "Major", notes: [7,11,2]},
+        "Vi" => {type: "Major", notes: [9,0,4]},
+        "Vii" => {type: "Minor", notes: [11,2,5]}
+    }
+    attr_reader :chord, :scale, :root
+    def initialize(root:, scale:)
+        @root = NOTES[root]
+        @scale = CHORDS[scale]
+        chords_notes = self.scale.notes
+        @chord = []
+        self.make_chord(base, chord_notes)
+    end
+    def make_chord(base, chord_notes)
+        chord_notes.each do |c|
+            note = c + base 
+            note_value = note >= 12 ? (note % 12) : note 
+            letter = NOTES.index(note_value)
+            self.chord << letter
+        end
+    end
 
-    
 end
