5dc240d8f481bdd69e93b099fe0ef93440931347
Nate <7nater.nelson@gmail.com>
2021-02-04 12:21:39 -0700

    CLI.main_menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,13 +1,13 @@
 class CLI
-    DELAY = 3
+    DELAY = .5
     def initialize
         puts
-        display_message(self.class.welcome_message)
+        display_message(self.welcome_message, true)
         puts
-        display_message(self.class.main_menu,false)
+        self.main_menu
     end
 
-    def self.welcome_message
+    def welcome_message
         ["Welcome to my app!","This app help you construct a chord scafold for a music score." , "Here you can puruse commonly used chord progression, create your own progressions and build your own score based off the progressions of your choice."] 
     end
     def mm_message
@@ -20,12 +20,49 @@ class CLI
         ]
     end
 
-    def display_message(array,delay = true)
+    def display_message(array,delay = false)
         array.each do |string|
             puts string
             sleep(DELAY) if delay == true
         end
     end
+
+    def help_message
+        [
+            "",
+            "To return to the main menu type 'main'",
+            "To exit out of the application, type 'exit'",
+            "For help, type 'help'",
+            ""
+        ]
+    end
+
+    def get_input
+        input = gets.chomp
+        if input.downcase == "help"
+            display_message(help_message)
+            self.get_input
+        end
+        input
+    end
+
+    def main_menu
+        display_message(self.mm_message, false)
+        case get_input
+        when "1"
+        when "2"
+        when "3"
+        when "main"
+            self.main_menu
+        when "exit"
+            self.exit_app
+        else
+            puts "Sorry, not a valid choice"
+            display_message(help_message)
+            self.main_menu
+        end
+    end
+
 
 
 end
